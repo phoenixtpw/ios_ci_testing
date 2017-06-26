8a8139f8eae87e7320a1434f9d00182887f30540
xiaoma-max <xiaoma_max@hotmail.com>
2017-06-26 16:45:35 +1000

    upload file test

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -1,7 +1,17 @@
-# Skip uploading if it is not on master. you can mark this if you don't need it
-if [ "${TRAVIS_BRANCH}" = "development" ]; then
-    echo "Testing on a development branch."
+#!/bin/sh
+
+#if [ "${TRAVIS_BRANCH}" = "development" ]; then
+#    echo "Testing on a development branch."
+#fi
+
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+echo "This is a pull request. No deployment will be done."
+exit 0
+fi
+if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+echo "Testing on a branch other than master. No deployment will be done."
+exit 0
 fi
 
-fastlane ios beta
+#fastlane ios beta
 
