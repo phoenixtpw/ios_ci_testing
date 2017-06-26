4975b77b62676cb725c517ce42fcd60ab6ba1a12
xiaoma-max <xiaoma_max@hotmail.com>
2017-06-26 13:57:50 +1000

    archive 3

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -1,10 +1,4 @@
 #xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
-#xcodebuild -exportArchive -exportFormat ipa -archivePath "$OUTPUTDIR/$APP_NAME.xcarchive" -exportPath "$OUTPUTDIR/$APP_NAME.ipa" -exportProvisioningProfile "$PROVISIONING_PROFIL"
-#
-#xcodebuild archive -workspace "$APP_NAME.xcworkspace" -scheme restcomm-olympus -configuration Release -derivedDataPath ./build -archivePath ./build/Products/restcomm-olympus.xcarchive
-
-- xcodebuild clean archive -archivePath "CI Test.xcodeproj" -scheme "CI Test"
-- xcodebuild -exportArchive -exportFormat ipa -archivePath "CI Test.xcarchive" -exportPath "CI Test.ipa" -exportProvisioningProfile "$PROVISIONING_PROFIL"
 
 IPA_PATH="$OUTPUTDIR/$APP_NAME.ipa"
 EMAILS="po-wei.tsai@appscore.com.au, mark.fulton@appscore.com.au"
