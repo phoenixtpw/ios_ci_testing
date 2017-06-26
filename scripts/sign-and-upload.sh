6d7201ab8a3260febde3a2b21218a815dac1461b
xiaoma-max <xiaoma_max@hotmail.com>
2017-06-26 11:50:50 +1000

    Max setup test

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -1,4 +1,4 @@
-#xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
 
 IPA_PATH="$OUTPUTDIR/$APP_NAME.ipa"
 EMAILS="po-wei.tsai@appscore.com.au, mark.fulton@appscore.com.au"
