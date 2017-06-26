f90a7f9de5916945da7fa21f99f9ee3ce7916de7
xiaoma-max <xiaoma_max@hotmail.com>
2017-06-26 15:33:19 +1000

    fastlane 3

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -1,5 +1,5 @@
 # Skip uploading if it is not on master. you can mark this if you don't need it
 if [[ "$TRAVIS_BRANCH" == "development" ]]; then
     echo "Testing on a development branch."
-    fastlane beta
 fi
+fastlane beta
