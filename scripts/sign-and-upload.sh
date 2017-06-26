2bf98044945482950959b3ce9113b5c4a21cb51c
xiaoma-max <xiaoma_max@hotmail.com>
2017-06-26 13:18:16 +1000

    archive test 1

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -1,4 +1,5 @@
-xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+#xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+xcodebuild -exportArchive -exportFormat ipa -archivePath "$OUTPUTDIR/$APP_NAME.xcarchive" -exportPath "$OUTPUTDIR/$APP_NAME.ipa" -exportProvisioningProfile "$PROVISIONING_PROFIL"
 
 IPA_PATH="$OUTPUTDIR/$APP_NAME.ipa"
 EMAILS="po-wei.tsai@appscore.com.au, mark.fulton@appscore.com.au"
