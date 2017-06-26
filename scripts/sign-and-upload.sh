5bed8123c57a99a4d6d4abd86f7c00589b0407b6
Phoenix Tsai <phoenixtpw@gmail.com>
2017-06-26 10:46:34 +1000

    +60

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -1,8 +1,4 @@
-xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+#xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
 
 IPA_PATH="$OUTPUTDIR/$APP_NAME.ipa"
 EMAILS="po-wei.tsai@appscore.com.au, mark.fulton@appscore.com.au"
-
-./travis/submit $FABRIC_API_KEY $FABRIC_BUILD_SECRET \
--ipaPath $IPA_PATH -emails $EMAILS \
--notifications YES
